759852e98842a4da0c79c9737e4c4325c5e5e25f
J <j@openmesh.network>
2024-08-08 16:13:50 +1000

    always take first parameter as script output path
    Signed-off-by: J <j@openmesh.network>

--- src/xnode-personaliser.sh
+++ src/xnode-personaliser.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-cat /proc/cmdline | grep -oE 'XNODE_PERSONALISER_SCRIPT=([a-zA-Z0-0+=/]*)' | cut -d= -f2- | base64 -d > xnode-personaliser-script.sh
-chmod +x xnode-personaliser-script.sh
-exec xnode-personaliser-script.sh
+cat /proc/cmdline | grep -oE 'XNODE_PERSONALISER_SCRIPT=([a-zA-Z0-0+=/]*)' | cut -d= -f2- | base64 -d > ${1}/xnode-personaliser-script.sh
+chmod +x ${1}/xnode-personaliser-script.sh
+exec ${1}/xnode-personaliser-script.sh
 
 exit 0
