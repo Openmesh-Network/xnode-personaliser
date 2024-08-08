170760443bf2202df8b4174b46e07f5d3a6a16d9
J <j@openmesh.network>
2024-08-08 13:01:21 +1000

    Package for nix flakes/legacy, add nix/flake.lock github workflow checks.
    Signed-off-by: J <j@openmesh.network>

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A well-defined environemnt script importer and execution engine for XnodeOS/Xnode integration environments";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pkg-name = "xnode-personaliser";
+        pkg-buildInputs = with pkgs; [ jq curl ];
+        pkg-script = (pkgs.writeScriptBin pkg-name (builtins.readFile ./src/xnode-personaliser.sh)).overrideAttrs(old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs ''$out";
+        });
+      in rec {
+        defaultPackage = packages.xnode-personaliser;
+        packages.xnode-personaliser = pkgs.symlinkJoin {
+          name = pkg-name;
+          paths = [ pkg-script ] ++ pkg-buildInputs;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${pkg-name} --prefix PATH : $out/bin";
+        };
+      }
+    );
+}
