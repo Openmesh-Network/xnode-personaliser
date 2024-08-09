b58c7e0f1909344d2540690c39ba914bd09437f2
J <j@openmesh.network>
2024-08-09 13:00:40 +1000

    Address shellcheck linter warnings/errors

--- src/xnode-personaliser.sh
+++ src/xnode-personaliser.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-cat /proc/cmdline | grep -oE 'XNODE_PERSONALISER_SCRIPT=([a-zA-Z0-0+=/]*)' | cut -d= -f2- | base64 -d > ${1}/xnode-personaliser-script.sh
-chmod +x ${1}/xnode-personaliser-script.sh
-exec ${1}/xnode-personaliser-script.sh
+grep -oE 'XNODE_PERSONALISER_SCRIPT=([a-zA-Z0-0+=/]*)' /proc/cmdline | cut -d= -f2- | base64 -d > "${1}/xnode-personaliser-script.sh"
+chmod +x "${1}/xnode-personaliser-script.sh"
+exec "${1}/xnode-personaliser-script.sh"
 
 exit 0
