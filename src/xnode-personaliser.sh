8bfeeb69d3e9057130ba1ec728f7db02a741f8af
dowdt <tomas.dowd@openmesh.network>
2024-08-12 14:48:36 +1000

    Fixed linting error :p.

--- src/xnode-personaliser.sh
+++ src/xnode-personaliser.sh
@@ -2,11 +2,11 @@
 
 SCRIPT_CONTENTS=$( grep -oE 'XNODE_PERSONALISER_SCRIPT=\S+' /proc/cmdline | cut -d= -f2- )
 
-echo "Found contents: " $SCRIPT_CONTENTS
+echo "Found contents: $SCRIPT_CONTENTS"
 
-echo $SCRIPT_CONTENTS | base64 -d | gzip -d 2>/dev/null > "${1}/xnode-personaliser-script.sh"
+echo "$SCRIPT_CONTENTS" | base64 -d | gzip -d 2>/dev/null > "${1}/xnode-personaliser-script.sh"
 
-echo "Running: " $(cat "${1}/xnode-personaliser-script.sh")
+echo "Running: $(cat "${1}/xnode-personaliser-script.sh")"
 
 chmod +x "${1}/xnode-personaliser-script.sh"
 exec "${1}/xnode-personaliser-script.sh"
